7fe6c2085042bf203a21f785c54d43345afab755
Rozelin OLĞAÇ <rolgac@innova.com.tr>
2024-06-08 08:23:49 +0300

    08.06

--- src/main/java/Arrays/Pratik.java
+++ src/main/java/Arrays/Pratik.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class Main {
+public class Pratik {
     static void printArray(double[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println((i + 1) + ". indeks : " + arr[i]);
